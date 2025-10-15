5777829d798329b9509ad2a347b8c70d1a25bfe7
magnite5 <magnite50@gmail.com>
2025-10-15 17:49:21 -0400

    Unfinished Title Management

--- src/main/java/dev/magnoix/msa/databases/PluginDatabase.java
+++ src/main/java/dev/magnoix/msa/databases/PluginDatabase.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class PluginDatabase {
+public class PluginDatabase { //TODO: Enable foreign keys
 
     private final Connection connection;
     private final StatisticsManager statisticsManager;
